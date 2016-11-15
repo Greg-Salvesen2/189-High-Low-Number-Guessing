ae05e3e6d7850e58622ace5f8b909023a61f17b5
Greg Salvesen <greg@latenightdesigns.com>
2016-11-15 13:40:46 -0500

    Initial Commit

---
+++ 189 High Low Number Guessing/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  189 High Low Number Guessing
+//
+//  Created by Greg Salvesen on 11/15/16.
+//  Copyright © 2016 DVS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    std::cout << "Hello, World!\n";
+    return 0;
+}
