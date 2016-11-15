452e0d2aabb63491b90e1803444927645af3c9fd
Greg Salvesen <greg@latenightdesigns.com>
2016-11-15 13:50:13 -0500

    Added the game functionality

--- 189 High Low Number Guessing/main.cpp
+++ 189 High Low Number Guessing/main.cpp
@@ -6,9 +6,27 @@
 //  Copyright © 2016 DVS. All rights reserved.
 //
 
+#include <cstdlib>
 #include <iostream>
+#include <ctime>
 
 int main(int argc, const char * argv[]) {
-    std::cout << "Hello, World!\n";
+    //Seed random number with the current time
+    srand((int) std::time(0));
+    int rand = (std::rand() % 100) + 1;
+    int guess = -1;
+    int guesses = 0;
+    while(guess != rand) {
+        std::cout << "Enter a guess between 1-100: ";
+        std::cin >> guess;
+        guesses++;
+        if(guess > rand) {
+            std::cout << "That guess is too high!" << std::endl;
+        } else if(guess < rand) {
+            std::cout << "That guess is too low!" << std::endl;
+        } else {
+            std::cout << "Congratulations! You guessed " << rand << " correctly in " << guesses << " guesses." << std::endl;
+        }
+    }
     return 0;
 }
